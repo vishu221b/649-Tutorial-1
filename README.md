95725979391145dbcda1b2073440ee0529406feb
vishu221b <vishal.dogra738@gmail.com>
2023-08-31 19:43:36 +1000

    Further refactoring ->  Moved assessment code, tier wise, into different directories, added the missing cloud tier publisher script for publishing user commands through downstream communication

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# 649-Tutorial-1
-First tutorial for IFN649
+# 649-Tutorial-Content
+Tutorial content for IFN649
+
+## Components
+- Arduino/Teensy Hello Serial
+- Bluetooth connection using Arduino/Teensy
+- DHT11 Sensor Interfacing with Arduino/Teensy
+- Soil moisture sensor interfacing with Arduino/Teensy
+- Communicating over Bluetooth serial between Raspberry pi and Teensy using Python and Serial1 respectively
+- MQTT Publish/Subscribe Topics
+- Full flow upstream/downstream communication over the three tiers (Sensor, Edge and Cloud) using MQTT, Raspberry Pi and Teensy (Week 6 Assessment)
+
---
+++ Tut-6-Assessment/Assessment Presentation Code /Cloud Tier/Downstream Publisher/downstream_command_publish.py
@@ -0,0 +1,17 @@
+import paho.mqtt.publish as publisher
+
+TEENSY_1_CONTROLLER_TOPIC="controller/teensy/1"
+MQTT_BROKER_HOST="0.0.0.0" #Change this to the EC2 MQTT broker instance url accordingly
+
+try:
+    while True:
+        _user_command=input("Input your command(enter q for quitting): ")
+        if _user_command.lower()  == 'q':
+            print("Exiting ...")
+            exit(1)
+        publisher.publish(TEENSY_1_CONTROLLER_TOPIC, _user_command, hostname=MQTT_BROKER_HOST)
+        print("Command published successfully!!")
+except KeyboardInterrupt:
+    print("\nExiting gracefully....")
+    exit(1)
+
